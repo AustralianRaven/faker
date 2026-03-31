f918042a83db18c84dd601443434a7b17abae584
Shinigami92 <chrissi92@hotmail.de>
2026-03-31 16:49:36 +0200

    fix cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,3 +1,4 @@
+import webpackPreprocessor from '@cypress/webpack-preprocessor';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
@@ -8,5 +9,33 @@ export default defineConfig({
     baseUrl: 'http://localhost:5173',
     supportFile: false,
     fixturesFolder: false,
+    setupNodeEvents(on) {
+      on(
+        'file:preprocessor',
+        webpackPreprocessor({
+          webpackOptions: {
+            resolve: {
+              extensions: ['.ts', '.js'],
+            },
+            module: {
+              rules: [
+                {
+                  test: /\.tsx?$/,
+                  exclude: [/node_modules/],
+                  use: [
+                    {
+                      loader: 'esbuild-loader',
+                      options: {
+                        target: 'es2015',
+                      },
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+        })
+      );
+    },
   },
 });
