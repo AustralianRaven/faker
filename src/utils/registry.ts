21c0a5ed28bfa993587d779be513bd31cbbd94e8
ST-DDT <ST-DDT@gmx.de>
2026-04-12 00:05:35 +0200

    utils registry

---
+++ src/utils/registry.ts
@@ -0,0 +1,7 @@
+import { getDefaultRefDate } from './get-default-ref-date';
+import { setDefaultRefDate } from './set-default-ref-date';
+
+export const utilsModule = {
+  getDefaultRefDate,
+  setDefaultRefDate,
+};
