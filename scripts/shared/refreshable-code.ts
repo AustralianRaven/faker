383ce0a32dab0a6ddab1e5f92ce57b6ac1e25fe7
ST-DDT <ST-DDT@gmx.de>
2026-04-12 00:05:15 +0200

    docs: fix examples

--- scripts/shared/refreshable-code.ts
+++ scripts/shared/refreshable-code.ts
@@ -2,25 +2,38 @@ import { formatTypescript } from '../shared/format';
 
 export async function toRefreshableCode(
   name: string,
-  exampleCode: string
+  exampleCode: string,
+  moduleHints: Record<string, string> = {}
 ): Promise<string> {
-  if (!/^\w*faker\w*\./im.test(exampleCode)) {
-    // No recordable faker calls in examples
-    return 'undefined';
-  }
-
   const exampleLines = exampleCode
     .replaceAll(/ ?\/\/.*$/gm, '') // Remove comments
     .replaceAll(/^import .*$/gm, '') // Remove imports
+    .replaceAll(/\b(?<!\.)(\w+)\(fakerCore/g, (match, p1) => {
+      // Access SMF via module registry if possible
+      if (moduleHints[p1]) {
+        return `moduleRegistry.${moduleHints[p1]}.${match}`;
+      }
+
+      throw new Error(
+        `Unable to find module hint for ${p1} in example code for ${name}`
+      );
+    })
     .replaceAll(
-      // record results of faker calls
-      /^(\w*faker\w*\..+(?:(?:.|\n..)*\n[^ ])?\)(?:\.\w+)?);?$/gim,
+      // record results of relevant calls
+      // Keep in sync with docs/.vitepress/components/api-docs/refreshable-code.vue
+      /^((?<callBase>\w*faker\w*|moduleRegistry)\.(?<consumeToEOL>.+)(?<multiline>(?<consumeIndented>\n +.*)+(?<finalLine>\n[^ \n]+))?\));?$/gim,
       `try { result.push($1); } catch (error: unknown) { result.push(error instanceof Error ? error.name : 'Error'); }\n`
     );
+
+  if (!exampleLines.includes('try { result.push(')) {
+    // No recordable calls in examples
+    return 'undefined';
+  }
 
   const fullMethod = `async (): Promise<unknown[]> => {
 await enableFaker();
 const result: unknown[] = [];
+${exampleCode.includes('fakerCore') ? 'const fakerCore = faker.fakerCore;' : ''}
 
 ${exampleLines}
 
